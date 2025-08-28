93d8747e78ad002c97b286a00bf69d37011d3a8a
MicBacon <34489992+MicBacon@users.noreply.github.com>
2025-08-28 19:33:54 +0200

    added hdf5 to gitignore, dockerfile for sct model environment and

--- show-control-and-tell-master/config.py
+++ show-control-and-tell-master/config.py
@@ -1,8 +1,8 @@
 # COCO root path
-coco_root = 'datasets/coco/'
+coco_root = '../shared/data/coco_object/coco2014/'
 
 # Flickr30k root path
-flickr_root = 'datasets/flickr/'
+flickr_root = '../shared/data/flickr30k/Images/'
 
 # Flickr30kEntities root path
-flickr_entities_root = 'datasets/flickr/Flickr30kEntities/'
+flickr_entities_root = '../shared/data/flickr30k/dataset_flickr/datasets/flickr/Flickr30kEntities/'
