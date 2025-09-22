032e0f32bc73d7978c96d1f432af68837657a34a
MicBacon <34489992+MicBacon@users.noreply.github.com>
2025-09-22 18:29:46 +0200

    adjusted translate_evaluate script to new compute metrics script

--- mPLUG/translate_evaluate.py
+++ mPLUG/translate_evaluate.py
@@ -3,32 +3,14 @@ import translators as ts
 import os
 import csv
 from bert_score import score
-import sys
-import os
+import compute_metrics
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'nlg-eval'))
+CAPTION_COUNT = 1000
+RESULT_PATH = 'output/coco_caption_large/result/'
 
-try:
-    from nlgeval import NLGEval
-except ImportError as e:
-    print(f"Failed to import NLGEval: {e}")
-    import nlgeval
-    print(f"Available in nlgeval: {dir(nlgeval)}")
-    exit(1)
+_ = ts.preaccelerate_and_speedtest()
 
-# Bleu_1 also deactivates 2, 3 and 4
-# SPICE causes problem regarding java library
-metrics_to_omit = ['SPICE']
-caption_count = 3783 #3783 = max caption count
-
-# Only use metrics that don't require Theano
-nlgeval = NLGEval(metrics_to_omit=metrics_to_omit, no_skipthoughts=True, no_glove=True)
-
-#_ = ts.preaccelerate_and_speedtest()
-
-result_path = 'output/coco_caption_large/result/'
-
-with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as f:
+with open(os.path.join(RESULT_PATH, 'vqa_result_flickr30k_epoch4.json'), 'r') as f:
     data = json.load(f)
 
     os.makedirs("output/coco_caption_large/result_translated", exist_ok=True)
@@ -36,7 +18,7 @@ with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as
     r = open("output/coco_caption_large/result_translated/vqa_result_epoch10_rank0_translated_captions.json", "w")
     r.write('[')
     
-    for idx, item in enumerate(data[:caption_count]):
+    for idx, item in enumerate(data[:CAPTION_COUNT]):
        caption = item['pred_caption']
 
        # polish supported:
@@ -48,12 +30,11 @@ with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as
        # deepl -> high quality to translate but response slowly
        # argos -> open-source
 
-       #print('Attempting to translate "{}"...'.format(caption))
        if(caption == ""):
            caption = "a photo"
            print("found empty caption: \n", item)
 
-       caption_pl = ts.translate_text(caption, translator='google', to_language='pl')
+       caption_pl = ts.translate_text(caption, translator='deepl', to_language='pl')
 
        # calculate multilingual translation bert scores
        P, R, F1 = score(
@@ -73,7 +54,7 @@ with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as
        # check if polish references exist
        if(len(captions_pl) > 0):
            # calculate image captioning metrics
-           captioning_metrics = nlgeval.compute_individual_metrics(captions_pl, caption_pl)
+           captioning_metrics = compute_metrics.compute_metrics(captions_pl, caption_pl, image_paths=None)
 
            json.dump({"question_id": item['question_id'], "pred_caption": caption, "translated_caption": caption_pl,
                     "translation_scores":{"P":P.numpy().tolist(), "R":R.numpy().tolist(), "F1":F1.numpy().tolist()},
@@ -84,7 +65,7 @@ with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as
            if (idx % 100 == 0):
             print(f"Processed {idx} captions.")
 
-           if idx < caption_count - 1:
+           if idx < CAPTION_COUNT - 1:
             r.write(',\n')
 
     r.write('\n],')
@@ -94,12 +75,14 @@ with open(os.path.join(result_path, 'vqa_result_flickr30k_epoch4.json'), 'r') as
     with open("output/coco_caption_large/result_translated/vqa_result_epoch10_rank0_translated_captions.json", "r") as f:
         data = json.load(f)
         references = []
+        img_paths = []
         hypotheses = []
         for item in data:
             references.append(item['polish_references'])
             hypotheses.append(item['translated_caption'])
-        scores = nlgeval.compute_metrics(references, hypotheses)
-        #json.dump({"overall_scores": scores}, r, ensure_ascii=False)
+            img_paths.append("Images/" + item['question_id'])
+        scores = compute_metrics.compute_metrics(references, hypotheses, img_paths)
+        json.dump({"overall_scores": scores}, r, ensure_ascii=False)
         print(scores)
     r.write(']')
     r.close()
