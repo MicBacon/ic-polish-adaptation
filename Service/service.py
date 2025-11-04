f7716ed0b6af3b1faa7aff34cf5278bbf46e6d3b
MicBacon <34489992+MicBacon@users.noreply.github.com>
2025-11-04 22:52:05 +0100

    corrected error handling

--- Service/service.py
+++ Service/service.py
@@ -8,16 +8,16 @@ QWEN_URL = "http://gradio-qwen-ctn:7862/generate_caption"
 
 def produce_caption(image_pil, variant_name):
     if not image_pil:
-        return 'You must upload an image.'
+        return '[ERROR] You must upload an image.'
     if not variant_name:
-        return 'You must specify a variant in dropdownlist.'
+        return '[ERROR] You must specify a variant in dropdownlist.'
     
     if 'mplug' in variant_name:
         url = MPLUG_URL
     elif 'qwen' in variant_name:
         url = QWEN_URL    
     else:
-        return 'Variant not supported. Contact author.'
+        return '[ERROR] Variant not supported. Contact author.'
 
     img_byte_arr = io.BytesIO()
     image_pil.save(img_byte_arr, format='PNG')
@@ -28,14 +28,14 @@ def produce_caption(image_pil, variant_name):
         data  = {'model': variant_name}
         response = requests.post(url, files=files, data=data, timeout=120)
         if response.status_code == 200:
-            return response.json().get("caption", "Error getting json response.")
+            return response.json().get("caption", "[ERROR] Error getting json response.")
         else:
-            return f"Not OK ({response.status_code}): {response.text}"
+            return f"[ERROR] Not OK ({response.status_code}): {response.text}"
             
     except requests.exceptions.ConnectionError:
-        return f"Can't connect to {variant_name} server."
+        return f"[ERROR] Can't connect to {variant_name} server."
     except Exception as e:
-        return f"Unexpected error: {e}"
+        return f"[ERROR] {e}"
     
 CHOICES = [
     ("mPLUG (Flickr30k only)", "mplug-flickr-only"),
